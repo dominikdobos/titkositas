515e440004766ae9d217c9ac8ce2b2acc9091fab
dominikdobos <dominik.dobos33@gmail.com>
2023-10-29 03:16:48 +0100

    Csak nagybetűst fogad el

---
+++ main.py
@@ -0,0 +1,4 @@
+import titkositas
+
+titkositas.titkosit()
+titkositas.dekodol()
---
+++ titkositas.py
@@ -0,0 +1,40 @@
+ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ,.:?! "
+titkosABC = "XYZABCDEFGHIJKLMNOPQRSTUVW,.:?! "
+
+
+def titkosit():
+    szoveg = input("Kérek egy szöveget: ")
+    titkosSzoveg = ""
+    for i in range(len(szoveg)):
+        match = False
+        aktBetu = szoveg[i]
+        poz = 0
+        j = 0
+        while match == False:
+            if aktBetu == ABC[j]:
+                poz = j
+                match = True
+            else:
+                j += 1
+        titkosBetu = titkosABC[poz]
+        titkosSzoveg += titkosBetu
+    print("A titkosított szöveg:", titkosSzoveg)
+
+
+def dekodol():
+    titkosSzoveg = input("Kérek egy TITKOS szöveget: ")
+    dekodoltSzoveg = ""
+    for i in range(len(titkosSzoveg)):
+        match = False
+        aktBetu = titkosSzoveg[i]
+        poz = 0
+        j = 0
+        while match == False:
+            if aktBetu == titkosABC[j]:
+                poz = j
+                match = True
+            else:
+                j += 1
+        dekodoltBetu = ABC[poz]
+        dekodoltSzoveg += dekodoltBetu
+    print("A titkosított szöveg:", dekodoltSzoveg)
